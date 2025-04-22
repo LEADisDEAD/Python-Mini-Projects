f6b6fa73b11ecf16b54eb5fc831d59a1e9d55c3b
Prathmesh Bajpai <prathmeshbajpai123@gmail.com>
2025-04-22 14:48:55 +0530

    Caesar cypher version 1

---
+++ main.py
@@ -0,0 +1,33 @@
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+output = ""
+
+
+choice = input("Encrypt or Decrypt").lower()
+if choice=="encrypt":
+    message = input("Enter your message:")
+    shift = int(input("Enter the shift :"))
+
+
+    for char in message:
+        if char in alphabet:
+            index = alphabet.index(char)
+            new_index = (index + shift) % 26
+            output += alphabet[new_index]
+        else:
+            output+= char
+
+    print(f"Encrypted message: {output}")
+else:
+    message = input("Enter your message:")
+    shift = int(input("Enter the shift :"))
+
+    for char in message:
+        if char in alphabet:
+            index = alphabet.index(char)
+            new_index = (index - shift) % 26
+            output += alphabet[new_index]
+        else:
+            output+= char
+
+    print(f"Encrypted message: {output}")
